288e72289ba366747fca136a1f297cf0bb3f78eb
some-user123 <oxymo@gmx.de>
2022-08-25 13:56:44 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    {{ state }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MyInterface } from '@/store/MyInterface.model';
+
+const state = ref<MyInterface>({ id: 1, msg: 'Hello World' });
+</script>
